c4b17ee88808a450f341c7e5dd9a77ce044f72c6
SK Yap <skorsengkuang@gmail.com>
2023-06-21 13:37:47 +1000

    Fixed bugs with about cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc'
 
 const ServiceCard = ({index, title, icon }) => (
-  <Tilt className = 'xs: w-[250px] w-full'>
+  <Tilt className = 'xs:w-[250px] w-full'>
     <motion.div 
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
       className = 'w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
